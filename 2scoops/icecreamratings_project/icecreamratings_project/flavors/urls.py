e57dfaf6b3aa538803c11b705aa751862513b6d8
Cristina Botez <cristina.botez@3pillarglobal.com>
2014-06-04 13:41:24 +0300

    Used CBVs to edit and view flavors. Added search flavors

--- 2scoops/icecreamratings_project/icecreamratings_project/flavors/urls.py
+++ 2scoops/icecreamratings_project/icecreamratings_project/flavors/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, url
 
-from .views import FlavorDetailView, FlavorCreateView
+from .views import (
+    FlavorDetailView, FlavorCreateView, FlavorUpdateView, FlavorListView
+)
 
 
 urlpatterns = patterns(
     '',
     url(r'^(?P<pk>\d+)/$', FlavorDetailView.as_view(), name='flavor_details'),
-    url(r'^add/$', FlavorCreateView.as_view(), name='flavor_create')
+    url(r'^add/$', FlavorCreateView.as_view(), name='flavor_create'),
+    url(r'^update/(?P<pk>\d+)/$', FlavorUpdateView.as_view(),
+        name='flavor_update'),
+    url(r'^$', FlavorListView.as_view(), name='flavor_list')
 )
--- 2scoops/icecreamratings_project/icecreamratings_project/flavors/views.py
+++ 2scoops/icecreamratings_project/icecreamratings_project/flavors/views.py
@@ -1,9 +1,26 @@
 from braces.views import LoginRequiredMixin
 
+from django.contrib import messages
 from django.core.urlresolvers import reverse
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView, ListView
 
 from .models import Flavor
+
+
+class FlavorActionMixin(object):
+
+    @property
+    def action(self):
+        msg = '{0} is missing action.'.format(self.__class__)
+        raise NotImplementedError(msg)
+
+    def form_valid(self, form):
+        msg = 'Flavor {0}!'.format(self.action)
+        messages.info(self.request, msg)
+        return super(FlavorActionMixin, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('flavor_details', kwargs={'pk': self.object.pk})
 
 
 class FlavorDetailView(LoginRequiredMixin, DetailView):
@@ -11,9 +28,26 @@ class FlavorDetailView(LoginRequiredMixin, DetailView):
     model = Flavor
 
 
-class FlavorCreateView(LoginRequiredMixin, CreateView):
+class FlavorCreateView(LoginRequiredMixin, FlavorActionMixin, CreateView):
+
+    model = Flavor
+    action = 'created'
+
+
+class FlavorUpdateView(LoginRequiredMixin, FlavorActionMixin, UpdateView):
+
+    model = Flavor
+    template_name = 'flavors/flavor_update.html'
+    action = 'updated'
+
+
+class FlavorListView(ListView):
 
     model = Flavor
 
-    def get_success_url(self):
-        return reverse('flavor_details', kwargs={'pk': self.object.pk})
+    def get_queryset(self):
+        result = super(FlavorListView, self).get_queryset()
+        q = self.request.GET.get('q')
+        if q is not None:
+            result = result.filter(name__icontains=q)
+        return result
